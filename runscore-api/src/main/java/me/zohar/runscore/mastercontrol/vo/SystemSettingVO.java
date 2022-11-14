913ce4cd6992d1182d70c15e872cfb8a950b81dc
wenyonghua <xiao_xiao8230@126.com>
2022-11-14 23:21:42 +0800

    提交最新代码

--- runscore-api/src/main/java/me/zohar/runscore/mastercontrol/vo/SystemSettingVO.java
+++ runscore-api/src/main/java/me/zohar/runscore/mastercontrol/vo/SystemSettingVO.java
@@ -33,7 +33,7 @@ public class SystemSettingVO {
 	/**
 	 * 最近修改时间
 	 */
-	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+7")
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
 	private Date latelyUpdateTime;
 
 	public static SystemSettingVO convertFor(SystemSetting setting) {
